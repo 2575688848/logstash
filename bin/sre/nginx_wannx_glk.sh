b8491eb3a081f160bdb39dd9c19b6da073acf2ac
姚太平 <yaotaiping@higohappy.com>
2020-07-01 17:24:00 +0800

    MOD: 修改sre方向logstash配置文件

--- bin/sre/nginx_wannx_glk.sh
+++ bin/sre/nginx_wannx_glk.sh
@@ -1,4 +1,4 @@
 
 #!/bin/bash
 
-nohup /home/work/elk/logstash-7.5.0/bin/logstash -f /home/work/elk/logstash/conf/back/nginx_wannx_glk.conf --path.data=/home/work/elk/logstash-7.5.0/project-data/nginx_wannx_glk_data/  >/dev/null 2>&1 &
+nohup /home/work/elk/logstash-7.5.0/bin/logstash -f /home/work/elk/logstash/conf/sre/nginx_wannx_glk.conf --path.data=/home/work/elk/logstash-7.5.0/project-data/nginx_wannx_glk_data/  >/dev/null 2>&1 &
