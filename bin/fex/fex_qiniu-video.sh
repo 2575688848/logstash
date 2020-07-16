e06521b4a710f94580a8fc84ff71f78a3bf69504
姚太平 <yaotaiping@higohappy.com>
2020-07-16 11:58:06 +0800

    ADD: 添加 fex 启动脚本

--- bin/fex/fex_qiniu-video.sh
+++ bin/fex/fex_qiniu-video.sh
@@ -1,4 +1,4 @@
 
 #!/bin/bash
 
-nohup /home/work/elk/logstash-7.5.0/bin/logstash -f /home/work/elk/logstash/conf/back/fex_api-proxy.conf --path.data=/home/work/elk/logstash-7.5.0/project-data/fex_api-proxy_data/  >/dev/null 2>&1 &
+nohup /home/work/elk/logstash-7.5.0/bin/logstash -f /home/work/elk/logstash/conf/fex/fex_qiniu-video.conf --path.data=/home/work/elk/logstash-7.5.0/project-data/fex_qiniu-video_data/  >/dev/null 2>&1 &
