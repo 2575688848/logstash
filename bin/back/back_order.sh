246e78f2259b6c2b8148a01040b370a99a0fdeb7
姚太平 <yaotaiping@higohappy.com>
2020-07-16 12:21:55 +0800

    ADD: 添加 back  启动脚本

---
+++ bin/back/back_order.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+nohup /home/work/elk/logstash-7.5.0/bin/logstash -f /home/work/elk/logstash/conf/back/back_order.conf --path.data=/home/work/elk/logstash-7.5.0/project-data/back_order_data/  >/dev/null 2>&1 &
