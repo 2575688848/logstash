192a1a7cda6761685b65eef0e3832a423f660d0e
姚太平 <yaotaiping@higohappy.com>
2020-07-17 11:26:23 +0800

    MOD: 修改README.ME

--- bin/back/back_goods.sh
+++ bin/back/back_goods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-nohup /home/work/elk/logstash-7.5.0/bin/logstash -f /home/work/elk/logstash/conf/back/back_goods.conf --path.data=/home/work/elk/logstash-7.5.0/project-data/back_goods_data/  >/dev/null 2>&1 & 
+nohup /home/work/elk/logstash-7.5.0/bin/logstash -f /home/work/elk/logstash/conf/back/back_goods.conf --path.data=/home/work/elk/logstash-7.5.0/project-data/back_goods_data/  >/home/work/elk/logstash/logs/back_goods.log 2>&1 &
