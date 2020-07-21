02d06c54d300a46ad02f71ce24dc3a5b56de61da
姚太平 <yaotaiping@higohappy.com>
2020-07-21 15:06:42 +0800

    ADD：增加输出日志到 nginx_lannx_all_service.log

--- bin/sre/nginx_lannx_all_service.sh
+++ bin/sre/nginx_lannx_all_service.sh
@@ -1,4 +1,4 @@
 
 #!/bin/bash
 
-nohup /home/work/elk/logstash-7.5.0/bin/logstash -f /home/work/elk/logstash/conf/sre/nginx_lannx_all_service.conf --path.data=/home/work/elk/logstash-7.5.0/project-data/nginx_lannx_all_service_data/  >/dev/null 2>&1 &
+nohup /home/work/elk/logstash-7.5.0/bin/logstash -f /home/work/elk/logstash/conf/sre/nginx_lannx_all_service.conf --path.data=/home/work/elk/logstash-7.5.0/project-data/nginx_lannx_all_service_data/  >/home/work/elk/logstash/logs/nginx_lannx_all_service.log 2>&1 &
