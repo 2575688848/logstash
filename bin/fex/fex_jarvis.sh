3faf1bfd0f7cea9b96ed8145a00a824e84807df8
姚太平 <yaotaiping@higohappy.com>
2020-07-24 14:33:30 +0800

    MOD：修改

--- bin/fex/fex_jarvis.sh
+++ bin/fex/fex_jarvis.sh
@@ -1,4 +1,4 @@
 
 #!/bin/bash
 
-nohup /home/work/elk/logstash-7.5.0/bin/logstash -f /home/work/elk/logstash/conf/fex/fex_jarvis.conf --path.data=/home/work/elk/logstash-7.5.0/project-data/fex_jarvis_data/  >/dev/null 2>&1 &
+nohup /home/work/elk/logstash-7.5.0/bin/logstash -f /home/work/elk/logstash/conf/fex/fex_jarvis.conf --path.data=/home/work/elk/logstash-7.5.0/project-data/fex_jarvis_data/  >/home/work/elk/logstash/logs/fex_jarvis.log 2>&1 &
